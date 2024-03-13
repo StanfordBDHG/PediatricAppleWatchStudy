e7fcb10c65fefcd71e2c393491ee201d30396fd0
Matthew Turk <mjturk@stanford.edu>
2024-03-13 08:29:05 -0700

    Unit tests for patient age group logic

--- PAWSTests/PAWSTests.swift
+++ PAWSTests/PAWSTests.swift
@@ -8,10 +8,31 @@
 
 @testable import PAWS
 import XCTest
+import Firebase
+import SpeziAccount
 
 
 class PAWSTests: XCTestCase {
-    func testExample() throws {
-        XCTAssertTrue(true)
+    func testAgeGroupIsAdultTrue() async throws {
+        var components = DateComponents()
+        components.year = 1970
+        components.month = 1
+        components.day = 1
+        components.hour = 0
+        components.minute = 0
+        components.second = 0
+        let dateOfBirth = Calendar.current.date(from: components)
+        XCTAssertNotNil(dateOfBirth)
+        let ageYears = Calendar.current.dateComponents([.year], from: dateOfBirth!, to: .now).year
+        XCTAssertNotNil(ageYears)
+        XCTAssertTrue(ageYears! >= 18)
+    }
+
+    func testAgeGroupIsAdultFalse() async throws {
+        let dateOfBirth = Calendar.current.date(byAdding: .year, value: -12, to: .now)
+        XCTAssertNotNil(dateOfBirth)
+        let ageYears = Calendar.current.dateComponents([.year], from: dateOfBirth!, to: .now).year
+        XCTAssertNotNil(ageYears)
+        XCTAssertFalse(ageYears! >= 18)
     }
 }
