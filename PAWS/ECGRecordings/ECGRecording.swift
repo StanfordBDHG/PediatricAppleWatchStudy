3985dd878a5f1d02dc6246b394eb52007df891c2
Matthew Turk <mjturk@stanford.edu>
2024-04-27 19:56:17 -0700

    Checkmark icon from SF Symbols

--- PAWS/ECGRecordings/ECGRecording.swift
+++ PAWS/ECGRecordings/ECGRecording.swift
@@ -13,16 +13,30 @@ import SwiftUI
 struct ECGRecording: View {
     let electrocardiogram: HKElectrocardiogram
     @State var symptoms: HKElectrocardiogram.Symptoms = [:]
+    @Environment(ECGModule.self) var ecgModule
     
     
     var body: some View {
         PAWSCard {
             VStack(alignment: .leading) {
-                Text("ECG Recording")
-                    .font(.title)
-                Text(electrocardiogram.endDate.formatted())
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("ECG Recording")
+                        .font(.title)
+                    HStack {
+                        Text(electrocardiogram.endDate.formatted())
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                        if ecgModule.isUploaded(electrocardiogram) {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundStyle(.green)
+                                .accessibilityLabel("Checkmark: ECG has been successfully uploaded")
+                        } else {
+                            ProgressView()
+                                .controlSize(.small)
+                                .padding(.horizontal, 1)
+                        }
+                    }
+                }
                 Divider()
                 if symptoms.isEmpty {
                     Text("Recorded no symptoms")
