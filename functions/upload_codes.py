1d420c271a0f6010518c2076a6546b9d608de0ef
Matthew Turk <mjturk@stanford.edu>
2024-05-12 15:57:33 -0700

    License files

--- functions/upload_codes.py
+++ functions/upload_codes.py
@@ -1,3 +1,11 @@
+#
+# This source file is part of the Stanford Spezi open-source project
+#
+# SPDX-FileCopyrightText: 2024 Stanford University and the project authors (see CONTRIBUTORS.md)
+#
+# SPDX-License-Identifier: MIT
+#
+
 import argparse
 import os
 import random
